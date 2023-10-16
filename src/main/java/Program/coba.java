8c8f9f59b8c1b132d6db5ed5b50c07d1ce39c6c0
medicahyono <ediuinmaliki@gmail.com>
2023-10-16 11:00:53 +0700

    create java sample code

---
+++ src/main/java/Program/coba.java
@@ -0,0 +1,10 @@
+package Program;
+
+public class coba
+{
+    public static void main (String []arg)
+    {
+        //ini adalah program untuk mencetak hello word
+        System.out.println("hello, World");
+    }
+}
